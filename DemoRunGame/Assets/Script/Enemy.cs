8b8679801e1ea2a0821bc3d04a586b7d4b72aa56
hirahirara <katan777@outlook.jp>
2025-09-04 23:45:01 +0900

    add file

---
+++ DemoRunGame/Assets/Script/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigidbody;
+    private GameObject player;
+    private GameController gameController;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = (player.transform.position - transform.position).normalized * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Debug.Log("dead");
+            gameController.GameOver();
+        }
+    }
+}
